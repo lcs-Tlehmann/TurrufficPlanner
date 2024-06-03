6455d7e5cc5871df6aff8f3fd61614621fdcf6f6
Tristan Lehmann <tlehmann@lcs.on.ca>
2024-06-03 14:42:28 -0400

    Initial Commit

---
+++ TurrufficPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TurrufficPlanner
+//
+//  Created by Tristan Lehmann on 2024-06-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TurrufficPlanner/TurrufficPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurrufficPlannerApp.swift
+//  TurrufficPlanner
+//
+//  Created by Tristan Lehmann on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct TurrufficPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
